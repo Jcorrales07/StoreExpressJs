b2d554ba4903f92c7203e420acfddd68a15864e3
Jcorrales07 <corralesjoe07@gmail.com>
2022-04-16 20:41:50 -0600

    Implementacion de cors para que otros origines se puedan conectar a mi API

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors')
+
 
 const { logErrors, errorHandler } = require('./middlewares/errorHandler');
 
@@ -9,6 +11,20 @@ const port = 3000;
 //Este es un middleware para que el POST pueda obtener la info del body
 app.use(express.json());
 //==>
+
+const whitelist = ['http://localhost:5500', 'https://myapp.co']
+const options = {
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('No permitido'))
+    }
+  }
+}
+
+
+app.use(cors())
 
 routerApi(app);
 //Implementacion de Middlewares de forma secuencial
